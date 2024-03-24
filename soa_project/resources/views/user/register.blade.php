512d46d48bb9df40080651a2de853b30dc5e24c7
Chomchanok99 <115713175+Chomchanok99@users.noreply.github.com>
2024-03-24 22:51:39 +0700

    เสร้จ register

--- soa_project/resources/views/user/register.blade.php
+++ soa_project/resources/views/user/register.blade.php
@@ -1,1 +1,23 @@
 <h1>register interface</h1>
+<div class="form-group">
+	<label for="">ชื่อ</label>
+	<input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+	<label for="">เบอร์โทร</label>
+	<input type="text" name="phon" class="form-control">
+</div>
+<div class="form-group">
+	<label for="">ที่อยู่</label>
+	<input type="text" name="้home" class="form-control">
+</div>
+<div class="form-group">
+	<label for="">Username</label>
+	<input type="text" name="Username" class="form-control">
+</div>
+<div class="form-group">
+	<label for="">Password</label>
+	<input type="text" name="Password" class="form-control">
+</div>
+<button class="btn btn-success" type="submit">ย้อนกลับ</button>
+<button class="btn btn-success" type="submit">ตกลง</button>
